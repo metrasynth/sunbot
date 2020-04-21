a7631d7a4fedf74c976763f4e35e990d733507e8
Matthew Scott <matt@11craft.com>
2020-04-21 15:40:59 -0500

    Initial commit: custom emoji reference auto-reactor

---
+++ sunbot.py
@@ -0,0 +1,74 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+
+FILTER_OVERRIDES = {
+    "filter": {"before": ["vocal"], "after": ["pro"]},
+    "generator": {"before": ["analog"]},
+}
+
+
+class SunBotClient(discord.Client):
+    def __init__(self):
+        super(SunBotClient, self).__init__()
+        self.guild_emoji = {}
+
+    async def on_ready(self):
+        guild: discord.Guild
+        emoji: discord.Emoji
+        for guild in self.guilds:
+            e = self.guild_emoji[guild.id] = {}
+            for emoji in guild.emojis:
+                ename = emoji.name.lower()
+                print(f"{guild.name} / {emoji.name} ({ename})")
+                e[ename] = emoji
+
+    async def on_message(self, message: discord.Message):
+        if message.author == client.user:
+            return
+        e = self.guild_emoji[message.guild.id]
+        searchtext = message.content.lower().replace(" ", "")
+        reactions_by_index = {}
+        for ename in e:
+            start = 0
+            while True:
+                try:
+                    idx = searchtext.index(ename, start)
+                except ValueError:
+                    break
+                start = idx + len(ename)
+                overrides = FILTER_OVERRIDES.get(ename)
+                override_found = False
+                if overrides:
+                    before = overrides.get("before", [])
+                    after = overrides.get("after", [])
+                    for override in before:
+                        if (first := idx - len(override)) >= 0:
+                            searchbefore = searchtext[first:idx]
+                            if searchbefore == override:
+                                override_found = True
+                                break
+                    if not override_found:
+                        for override in after:
+                            if (last := idx + len(override)) <= len(override):
+                                searchafter = searchtext[idx:last]
+                                if searchafter == override:
+                                    override_found = True
+                                    break
+                if not override_found:
+                    reactions_by_index[idx] = ename
+        reactions = [ename for idx, ename in sorted(reactions_by_index.items())]
+        if reactions:
+            print(f"Reacting to f{message.content!r} with {reactions}")
+        for ename in reactions:
+            await message.add_reaction(e[ename])
+
+
+if __name__ == "__main__":
+    load_dotenv("local.env")
+    TOKEN = os.getenv("BOT_TOKEN")
+    client = SunBotClient()
+
+    client.run(TOKEN)
