c222b5d4a54fbd7a3df784d3d6cbcc44486063b1
Matthew Scott <matt@11craft.com>
2020-04-24 16:16:46 -0500

    Render uploaded projects (does not yet transcode to ogg)

--- sunbot.py
+++ sunbot.py
@@ -1,12 +1,19 @@
 import logging
 import os
+from pathlib import Path
 from typing import List
 
 import discord
 from dotenv import load_dotenv
 from normality import ascii_text
+from rv.api import read_sunvox_file
+from sunvox.api import Slot
+from sunvox.buffered import BufferedProcess, float32
 
 log = logging.getLogger(__name__)
+
+
+FILES_PATH = Path(__file__).parent / "files"
 
 
 EMOJI_REACTIONS = {
@@ -132,9 +139,37 @@ class SunBotClient(discord.Client):
             await message.add_reaction(e[ename])
 
     async def _on_message_projectrenderer(self, message: discord.Message):
+        if os.getenv("PROJECTRENDERER") != "1":
+            return
         for attachment in message.attachments:
             if attachment.filename.lower().endswith(".sunvox"):
                 log.info("Found SunVox attachment of %d bytes", attachment.size)
+                file_path = FILES_PATH / str(attachment.id) / attachment.filename
+                file_path.parent.mkdir(parents=True, exist_ok=True)
+                with file_path.open("wb") as f:
+                    await attachment.save(f)
+                    log.info("Saved to %r", file_path)
+                project = read_sunvox_file(str(file_path))
+                channel: discord.TextChannel = message.channel
+                await channel.send(
+                    f"I found a SunVox Project, called {project.name!r}. "
+                    "I'll render it to an OGG file now and upload it here."
+                )
+                freq = 44100
+                p = BufferedProcess(freq=freq, size=freq, channels=2, data_type=float32)
+                slot = Slot(str(file_path), process=p)
+                length = slot.get_song_length_frames()
+                slot.play_from_beginning()
+                position = 0
+                while position < length:
+                    log.info("%r, %r", position, length)
+                    buffer = p.fill_buffer()
+                    one_second = position + freq
+                    end_pos = min(one_second, length)
+                    copy_size = end_pos - position
+                    if copy_size < one_second:
+                        buffer = buffer[:copy_size]
+                    position = end_pos
 
 
 def main():
